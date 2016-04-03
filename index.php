35a9e2c06339c89a044661070640b4577a4a275b
moeinrahimi <m.rahimi2150@gmail.com>
2016-04-03 21:44:33 +0430

    guzzle

--- index.php
+++ index.php
@@ -2,18 +2,22 @@
  ini_set('display_errors', 'On');
   error_reporting(E_ALL);
 require 'vendor/autoload.php';
-
 use GuzzleHttp\Client;
 
-$client = new Client(
-'https://stackexchange.com/');
- 
-
-$res = $client->request('POST', 'oauth', [
-    'query' => ['client_id' => '6738']
-]);
+	?>
 
 
+<a href=
+"https://stackexchange.com/oauth?client_id=6738&scope=read_inbox&redirect_uri=https://stacknotifier.herokuapp.com/"
+>sign me in</a>
 
 
-print_r($res);
+<?php
+$code = $_GET['code'];
+$client = new GuzzleHttp\Client(['base_uri' => 'https://stackexchange.com/oauth/access_token']);
+
+$response = $client->post("?client_id=6738&client_secret=hLhSGXL0yWCCtuAc7FORbQ&code=$code&redirect_uri= https://stacknotifier.herokuapp.com/")->send();
+
+print_r($response);
+
+?>
