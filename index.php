dfcce3752179606d4a6b8aeaef6af50f47d2fe46
moeinrahimi <m.rahimi2150@gmail.com>
2016-04-03 21:57:30 +0430

    h

--- index.php
+++ index.php
@@ -8,15 +8,18 @@ use GuzzleHttp\Client;
 
 
 <a href=
-"https://stackexchange.com/oauth?client_id=6738&scope=read_inbox&redirect_uri=https://stacknotifier.herokuapp.com/"
+"https://stackexchange.com/oauth/dialog?client_id=6738&scope=read_inbox&redirect_uri=https://stacknotifier.herokuapp.com/"
 >sign me in</a>
 
 
 <?php
-$code = $_GET['code'];
-$client = new GuzzleHttp\Client(['base_uri' => 'https://stackexchange.com/oauth/access_token']);
+$code = $_GET['access_token'];
+echo $code;
 
-$response = $client->post("?client_id=6738&client_secret=hLhSGXL0yWCCtuAc7FORbQ&code=$code&redirect_uri= https://stacknotifier.herokuapp.com/")->send();
+
+$client = new GuzzleHttp\Client(['base_uri' => 'https://api.stackexchange.com/']);
+
+$response = $client->post("2.2/me/notifications?site=stackoverflow&access_token=$code&scope=read_inbox")->send();
 
 print_r($response);
 
