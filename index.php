a24a85fe730a8e54ce16abf7823b4333fce09e08
moeinrahimi <m.rahimi2150@gmail.com>
2016-04-05 14:53:40 +0430

    access

--- index.php
+++ index.php
@@ -17,7 +17,8 @@ if (isset($_GET['code'])) {
 $client = new GuzzleHttp\Client();
 $code =$_GET['code'];
 $response = $client->post("https://stackexchange.com/oauth/access_token?client_id=6738&client_secret=hLhSGXL0yWCCtuAc7FORbQ((&code=$code&redirect_uri=https://stacknotifier.herokuapp.com/");
-print_r($response);
+
+print_r($response->send($response));
 }
 
 ?>
