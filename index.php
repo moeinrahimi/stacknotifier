d72c80005a2965691bc028ad0c12379da1bc5fc3
moeinrahimi <m.rahimi2150@gmail.com>
2016-04-05 17:32:48 +0430

    access

--- index.php
+++ index.php
@@ -46,7 +46,7 @@ $client = new Client([
 ]);
 echo $access_token;
 $response = $client->request('GET',"2.2/me/notifications?site=stackoverflow&access_token=$access_token&key=TxfA4E5YFX*jZ5wHEd2iKg((");
-$y = (string) $response->getBody()
+$y = (string) $response->getBody();
 print_r($y);
 
 }
